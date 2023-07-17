7669a4bb1007db244887ae6661977ba54ef95d7b
Moin Mulani <moinmulani00@gmail.com>
2023-07-17 15:06:28 +0530

    Constructors

--- constructors/src/typesofconstructor/Constructors.java
+++ constructors/src/typesofconstructor/Constructors.java
@@ -5,22 +5,22 @@ int id;
 String name;
 int age;
 
-    public Constructors(int id, String name, int age) {
+    public Constructors() {
         System.out.println("NO_ARGUMENT CONSTRUCTOR CALLED");
+    }
+
+    public Constructors(int id, String name, int age) {
+//        parameterised constructor
+        System.out.println("PARAMETERISED CONSTRUCTOR CALLED");
         this.id = id;
         this.name = name;
         this.age = age;
         System.out.println(toString());
     }
 
-    //Constructors(int id ,String name , int age){
-//    System.out.println("NO_ARGUMENT CONSTRUCTOR CALLED");
-//    this.id=id;
-//    this.name=name;
-//    this.age=age;
-//    System.out.println(toString());
-//}
+
 Constructors(Constructors constructor){
+//        COPY CONSTRUCTOR
     System.out.println("Copy constructor called");
     this.id= constructor.id;
     this.name= constructor.name;
--- constructors/src/typesofconstructor/Main.java
+++ constructors/src/typesofconstructor/Main.java
@@ -2,6 +2,9 @@ package typesofconstructor;
 
 public class Main {
     public static void main(String[] args) {
+
+        Constructors c = new Constructors();
+
         Constructors c1 =new Constructors(14,"Moin",23);
 
         Constructors c2 =new Constructors(c1);
