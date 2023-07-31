3cd787d34ccbf53c673c04a3421613db50e59129
Moin Mulani <moinmulani00@gmail.com>
2023-07-31 23:55:16 +0530

    1.Functional Interface 2.Anonymous class

---
+++ java 8 Features/src/functional_Interface1/Driver.java
@@ -0,0 +1,26 @@
+package functional_Interface1;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        ExampleInterface instanceAnonymousClass = new ExampleInterface() {
+            @Override
+            public int randomCalculate(int a, int b) {
+                System.out.println("randomCalculate() method called inside anonymous class!!!");
+                return 0;
+            }
+        };
+        instanceAnonymousClass.print(200);
+        instanceAnonymousClass.max();
+        instanceAnonymousClass.randomCalculate(10,20);
+
+        ExampleInterface instanceLambda = (a,b)-> {
+            System.out.println("lambda Expression a*b = "+a*b);
+            return 0;
+        };
+
+        instanceLambda.randomCalculate(12,12);
+
+
+    }
+}
---
+++ java 8 Features/src/functional_Interface1/ExampleInterface.java
@@ -0,0 +1,13 @@
+package functional_Interface1;
+@FunctionalInterface
+public interface ExampleInterface {
+    int randomCalculate(int a, int b);//method declaration for abstract method
+
+    default void print(int result) {
+        System.out.println("Concrete method of Functional Interface Called : "+result);
+    }//default method is a concrete method that written in functional interface.
+    default void max(){
+        System.out.println("Default/Concrete method creation = Max");
+    }
+
+}
